f2b1de45e220c27571b9b0e3fd1e8d14d7812282
Andres Lopez <ing.andlopgam5@gmail.com>
2025-11-16 19:51:14 -0800

    lesson 2 (strings)

---
+++ background/day1/2.string_lesson.py
@@ -0,0 +1,14 @@
+print("100 + 50")
+
+print("Hello " + "Andres")
+
+print("My name is \"Andres\"")
+
+print("This is a line\nAnd this is another line")
+
+print("\tThis is the third line with a tab")
+
+print('This isn\'t a number')
+
+print("This character \\ is an inverted slash")
+
