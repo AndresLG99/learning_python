4465bca466892ea866cee5a0546a75650da127af
Andres Lopez <ing.andlopgam5@gmail.com>
2025-11-17 18:44:48 -0800

    lesson 3 (input)

---
+++ background/day1/3.input_lesson.py
@@ -0,0 +1,10 @@
+print("What's your first name?")
+print("What's your last name?")
+
+input("What's your first name? ")
+input("What's your last name? ")
+
+print(input("What's your first name? "))
+print(input("What's your last name? "))
+
+print("Your name is " + input("What's your first name? ") + " " + input("What's your last name? "))
