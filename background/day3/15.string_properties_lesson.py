6685bf4e47edf8f8062a00dd032e9fafa071fe0c
Andres Lopez <ing.andlopgam5@gmail.com>
2025-11-18 23:04:16 -0800

    lesson 15 (string properties)

---
+++ background/day3/15.string_properties_lesson.py
@@ -0,0 +1,25 @@
+# Inmutables
+name = "Carina"
+# name[0] = "K"
+name = "Karina"
+print(name)
+
+# Concatenate
+n1 = "Kari"
+n2 = "na"
+print(n1 + n2)
+
+# Multiplicate
+print(n1 * 10)
+
+# Multiline
+poem = """Mil pequeños peces blancos
+comosi hirviera
+el color del agua"""
+
+# Check if there's a sub-string
+print("agua" in  poem)
+print("sol" not in poem)
+
+# Check length
+print(len(poem))
