df1fa335fe3f23416e23deeb873f9efa187e7e07
Andres Lopez <ing.andlopgam5@gmail.com>
2025-11-18 22:36:18 -0800

    lesson 13 (sub strings)

---
+++ background/day3/13.sub_strings_lesson.py
@@ -0,0 +1,15 @@
+my_text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+piece = my_text[2]
+print(piece)
+piece = my_text[2:5]
+print(piece)
+piece = my_text[2:]
+print(piece)
+piece = my_text[:5]
+print(piece)
+piece = my_text[2:10:2]
+print(piece)
+piece = my_text[::3]
+print(piece)
+piece = my_text[::-1]
+print(piece)
