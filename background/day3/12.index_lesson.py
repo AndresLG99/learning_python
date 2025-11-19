e8ceda219dacb756fb6ab36f40a769160eca7a0f
Andres Lopez <ing.andlopgam5@gmail.com>
2025-11-18 22:30:17 -0800

    lesson 12 (index)

---
+++ background/day3/12.index_lesson.py
@@ -0,0 +1,17 @@
+my_text = "This is a test"
+result = my_text[0]
+print(result)
+result = my_text[10]
+print(result)
+result = my_text[-4]
+print(result)
+result = my_text.index("a")
+print(result)
+result = my_text.index("test")
+print(result)
+result = my_text.index("t",11)
+print(result)
+result = my_text.index("i",1,4)
+print(result)
+result = my_text.rindex("t")
+print(result)
