70fcc6794e45ad342116233a5bbb5589f2bafcf7
Andres Lopez <ing.andlopgam5@gmail.com>
2025-11-27 16:13:23 -0800

    lesson 21 (comparison operators)

---
+++ background/day4/21.comparison_operators_lesson.py
@@ -0,0 +1,56 @@
+# >     more than
+# <     less than
+# >=    more or equal than
+# <=    less or equal than
+# ==    equal to
+# !=    different from
+
+my_bool = 10 == 10
+print(my_bool)
+print("-" * 50)
+print("")
+
+my_bool = 5 + 5 == 10
+print(my_bool)
+print("-" * 50)
+print("")
+
+my_bool = "white" == "white"
+print(my_bool)
+print("-" * 50)
+print("")
+
+my_bool = "white" == "black"
+print(my_bool)
+print("-" * 50)
+print("")
+
+my_bool = "black" == "Black"
+print(my_bool)
+print("-" * 50)
+print("")
+
+my_bool = "black" == "Black".lower()
+print(my_bool)
+print("-" * 50)
+print("")
+
+my_bool = "100" == 100
+print(my_bool)
+print("-" * 50)
+print("")
+
+my_bool = 100 == 100.0
+print(my_bool)
+print("-" * 50)
+print("")
+
+my_bool = 100 != 100.0
+print(my_bool)
+print("-" * 50)
+print("")
+
+my_bool = 5 >= 6
+print(my_bool)
+print("-" * 50)
+print("")
