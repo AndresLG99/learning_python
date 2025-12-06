aa23e7dfddeb53d997e3858d4cadd582e0feb15f
Andres Lopez <ing.andlopgam5@gmail.com>
2025-12-06 13:38:19 -0800

    lesson 33 (create functions)

---
+++ background/day5/33.create_functions_lesson.py
@@ -0,0 +1,15 @@
+def greet():
+    '''
+    This function works to greet someone
+    new line
+    '''
+    # This is a one line comment
+    print("Hello, world!")
+
+greet()
+
+def greet_name(name):
+    print(f"Hello {name}!")
+
+greet_name("Andres")
+
